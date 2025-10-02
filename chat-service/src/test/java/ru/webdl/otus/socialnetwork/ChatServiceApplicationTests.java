5a6aee4e8b62e410a88d5fd80e51b0a984811f37
Тимофеев Кирилл <tk@webdl.ru>
2025-10-02 15:13:47 +0300

    HW05: Инициализация chat-service

---
+++ chat-service/src/test/java/ru/webdl/otus/socialnetwork/ChatServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.webdl.otus.socialnetwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
